1a32c43813a9d9e92def8e5849de2bf71b974781
6ex06en <kerzo@mail.ru>
2015-05-08 09:26:19 +0300

    Update marker.rb

--- app/models/marker.rb
+++ app/models/marker.rb
@@ -2,7 +2,7 @@ class Marker < ActiveRecord::Base
   attr_accessible :address, :description, :latitude, :longitude, :title
   geocoded_by :address
   after_validation :geocode
-  belongs_to :my_maps
+  belongs_to :my_map
   validates :title, presence:true, uniqueness: true
   validates :latitude, presence:true
 end
